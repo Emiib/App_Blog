0c40d118a78d82d442390e4c180452a79483c683
Emiliano <emii88b@gmail.com>
2022-10-09 13:12:06 -0300

    git commit

--- BlogOtaku/wsgi.py
+++ BlogOtaku/wsgi.py
@@ -8,22 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
-from whitenoise import WhiteNoise
-
 from django.core.wsgi import get_wsgi_application
-
-from BlogOtaku import MyWSGIApp
-
-application = MyWSGIApp()
-
-application = WhiteNoise(application, root="/static")
-
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogOtaku.settings')
 
 application = get_wsgi_application()
 
+application = WhiteNoise(application)
 
 
 
