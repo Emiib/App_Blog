efb5cb86ebcc4a580f02666960720c6828a500ac
Emiliano <emii88b@gmail.com>
2022-10-10 16:27:59 -0300

    git commit

--- AppProfile/models.py
+++ AppProfile/models.py
@@ -1,10 +1,11 @@
+from email.policy import default
 from django.db import models
 
 from django.contrib.auth.models import User
 
 class Avatar(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/avatardefault.png', null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.user.username}"
