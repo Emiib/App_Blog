b043fab0418bd56511f471d483cde1f0648bce0b
Emiliano <emii88b@gmail.com>
2022-10-10 16:40:16 -0300

    git commit

--- AppBlog/forms.py
+++ AppBlog/forms.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 class NewPost(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('anime_name','sub_name','content', 'photo','category')
+        fields = ('anime_name','sub_name','content', 'photo','published_at','category')
         widgets = {'content': forms.Textarea(attrs={'required': True, 'cols': 80}),
                    'photo': forms.FileInput(),
                    'published_at': forms.DateInput(attrs={'type': 'date'})}
